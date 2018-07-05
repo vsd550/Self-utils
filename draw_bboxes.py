4136a974785f5f59ce8130cfc8a9f1de90e00f0d
vsd550 <vijayshankerdubey550@gmail.com>
2018-07-05 15:07:53 +0530

    Update draw_bboxes.py

--- draw_bboxes.py
+++ draw_bboxes.py
@@ -53,3 +53,14 @@ output_name_mod = os.path.basename(im_name) + '.' + 'jpg'
 fig2.savefig(os.path.join(output_dir, '{}'.format(output_name_mod)), dpi=dpi)
 
 plt.close('all')
+
+def get_ax(rows=1, cols=1, size=16):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Adjust the size attribute to control how big to render images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
