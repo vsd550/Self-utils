3fa9b8773e8a7914d7ed4d2a59c4e38086843bd0
vsd550 <vijayshankerdubey550@gmail.com>
2018-07-16 10:48:24 +0530

    Update draw_bboxes.py

--- draw_bboxes.py
+++ draw_bboxes.py
@@ -140,3 +140,25 @@ ll = []
 ll.append(a)
 ll.append(b)
 ll
+def bbox_iou(bbox_a, bbox_b):
+    """Calculate the Intersection of Unions (IoUs) between bounding boxes.
+    IoU is calculated as a ratio of area of the intersection
+    and area of the union.
+
+    bbox_a: numpy array
+    bbox_b: numpy array
+
+    Returns the area between two numpy array's.
+    """
+    if bbox_a.shape[1] != 4 or bbox_b.shape[1] != 4:
+        raise IndexError
+
+    # top left
+    tl = np.maximum(bbox_a[:, None, :2], bbox_b[:, :2])
+    # bottom right
+    br = np.minimum(bbox_a[:, None, 2:], bbox_b[:, 2:])
+
+    area_i = np.prod(br - tl, axis=2) * (tl < br).all(axis=2)
+    area_a = np.prod(bbox_a[:, 2:] - bbox_a[:, :2], axis=1)
+    area_b = np.prod(bbox_b[:, 2:] - bbox_b[:, :2], axis=1)
+    return area_i / (area_a[:, None] + area_b - area_i)
